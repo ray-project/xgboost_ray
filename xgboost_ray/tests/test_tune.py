f2fa002b1de2954c0a2add81d2cefa83fee0525d
Amog Kamsetty <amogkamsetty@yahoo.com>
2020-11-25 17:04:26 -0800

    formatting

--- xgboost_ray/tests/test_tune.py
+++ xgboost_ray/tests/test_tune.py
@@ -1,15 +1,11 @@
-import os
-import tempfile
 import unittest
 
 import numpy as np
-import pandas as pd
 
 import ray
 from ray import tune
 
 from xgboost_ray import RayDMatrix, train
-from xgboost_ray.matrix import concat_dataframes
 
 
 class XGBoostRayDMatrixTest(unittest.TestCase):
@@ -17,14 +13,12 @@ class XGBoostRayDMatrixTest(unittest.TestCase):
         ray.init(num_cpus=4)
         repeat = 8  # Repeat data a couple of times for stability
         x = np.array([
-                              [1, 0, 0, 0],  # Feature 0 -> Label 0
-                              [0, 1, 0, 0],  # Feature 1 -> Label 1
-                              [0, 0, 1, 1],  # Feature 2+3 -> Label 2
-                              [0, 0, 1, 0],  # Feature 2+!3 -> Label 3
-                          ] * repeat)
-        y = np.array([
-                              0, 1, 2, 3
-                          ] * repeat)
+            [1, 0, 0, 0],  # Feature 0 -> Label 0
+            [0, 1, 0, 0],  # Feature 1 -> Label 1
+            [0, 0, 1, 1],  # Feature 2+3 -> Label 2
+            [0, 0, 1, 0],  # Feature 2+!3 -> Label 3
+        ] * repeat)
+        y = np.array([0, 1, 2, 3] * repeat)
 
         self.params = {
             "xgb": {
@@ -48,18 +42,24 @@ class XGBoostRayDMatrixTest(unittest.TestCase):
 
         self.train_func = train_func
 
-
     def tearDown(self):
         ray.shutdown()
 
     # noinspection PyTypeChecker
     def test_num_iters(self):
-        analysis = tune.run(self.train_func, config=self.params,
-                            resources_per_trial={"cpu": 1,"extra_cpu": 1},
-                            num_samples=2)
+        analysis = tune.run(
+            self.train_func,
+            config=self.params,
+            resources_per_trial={
+                "cpu": 1,
+                "extra_cpu": 1
+            },
+            num_samples=2)
 
-        self.assertTrue(all(analysis.results_df["training_iteration"] ==
-                            analysis.results_df["config.num_boost_round"]))
+        self.assertTrue(
+            all(analysis.results_df["training_iteration"] ==
+                analysis.results_df["config.num_boost_round"]))
+
 
 if __name__ == "__main__":
     import pytest
