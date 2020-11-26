6434f60239d30dc86ae7841e23d258775f0a3e82
Kai Fricke <kai@anyscale.com>
2020-11-26 12:18:48 +0100

    Flake8 in format --all

--- format.sh
+++ format.sh
@@ -119,6 +119,7 @@ format_changed() {
 # Format all files, and print the diff to stdout for travis.
 format_all() {
     yapf --diff "${YAPF_FLAGS[@]}" "${YAPF_EXCLUDES[@]}" xgboost_ray
+    flake8 --inline-quotes '"' --no-avoid-escape --ignore=C408,E121,E123,E126,E211,E225,E226,E227,E24,E704,E999,W503,W504,W605 xgboost_ray
 }
 
 # This flag formats individual files. --files *must* be the first command line
