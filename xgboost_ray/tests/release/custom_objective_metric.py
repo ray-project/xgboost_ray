4090657daa68913d6766be11e4b2ed16ffb29108
Kai Fricke <kai@anyscale.com>
2021-01-15 23:46:41 +0100

    pip install in readme

--- xgboost_ray/tests/release/custom_objective_metric.py
+++ xgboost_ray/tests/release/custom_objective_metric.py
@@ -5,7 +5,6 @@ from xgboost_ray.tests.test_xgboost_api import XGBoostAPITest
 
 class XGBoostDistributedAPITest(XGBoostAPITest):
     def _init_ray(self):
-        self.kwargs = {"resources_per_actor": {"actor_cpus": 4.0}}
         if not ray.is_initialized():
             ray.init(address="auto")
 
@@ -13,4 +12,4 @@ class XGBoostDistributedAPITest(XGBoostAPITest):
 if __name__ == "__main__":
     import pytest
     import sys
-    sys.exit(pytest.main(["-v", __file__]))
+    sys.exit(pytest.main(["-v", f"{__file__}::XGBoostDistributedAPITest"]))
