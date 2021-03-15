05e1f41a9cccc9b79a1adf77629d34ff17c4104f
Kai Fricke <kai@anyscale.com>
2021-03-15 17:15:40 +0100

    Add cluster test

--- xgboost_ray/tests/test_data_source.py
+++ xgboost_ray/tests/test_data_source.py
@@ -2,8 +2,12 @@ import unittest
 from unittest.mock import patch
 
 import numpy as np
+import pandas as pd
 
 import ray
+
+from xgboost_ray.data_sources import Modin
+from xgboost_ray.main import RayXGBoostActor
 
 
 class ModinDataSourceTest(unittest.TestCase):
@@ -11,30 +15,30 @@ class ModinDataSourceTest(unittest.TestCase):
 
     def setUp(self):
         repeat = 8  # Repeat data a couple of times for stability
-        self.x = np.array([
-            [1, 0, 0, 0],  # Feature 0 -> Label 0
-            [0, 1, 0, 0],  # Feature 1 -> Label 1
-            [0, 0, 1, 1],  # Feature 2+3 -> Label 2
-            [0, 0, 1, 0],  # Feature 2+!3 -> Label 3
-        ] * repeat)
+        self.x = np.repeat(range(8), 16).reshape((32, 4))
         self.y = np.array([0, 1, 2, 3] * repeat)
 
-    @classmethod
-    def setUpClass(cls):
-        ray.init(num_cpus=1, local_mode=True)
+        self._init_ray()
 
-    @classmethod
-    def tearDownClass(cls):
-        ray.shutdown()
+    def tearDown(self) -> None:
+        if ray.is_initialized():
+            ray.shutdown()
 
-    def _testAssignPartitions(self, part_nodes, actors_to_node,
+    def _init_ray(self):
+        if not ray.is_initialized():
+            ray.init(num_cpus=1)
+
+    def _testAssignPartitions(self, part_nodes, actor_nodes,
                               expected_actor_parts):
         from xgboost_ray.data_sources.modin import assign_partitions_to_actors
 
         partitions = [ray.put(p) for p in np.split(self.x, len(part_nodes))]
 
-        part_to_node = dict(zip(partitions, part_nodes))
+        # Dict from partition (obj ref) to node host
+        part_to_node = dict(zip(partitions, [f"node{n}" for n in part_nodes]))
         node_to_part = [(ray.put(n), p) for p, n in part_to_node.items()]
+
+        actors_to_node = dict(enumerate(f"node{n}" for n in actor_nodes))
 
         def unwrap(data, *args, **kwargs):
             return data
@@ -53,36 +57,76 @@ class ModinDataSourceTest(unittest.TestCase):
                     msg=f"Assignment failed: Actor rank {actor_rank}, "
                     f"partition {i} is not partition with ID {part_id}.")
 
+    def _testModinAssignment(self, part_nodes, actor_nodes,
+                             expected_actor_parts):
+        node_ips = [
+            node["NodeManagerAddress"] for node in ray.nodes() if node["Alive"]
+        ]
+        if len(node_ips) < max(max(actor_nodes), max(part_nodes)):
+            print("Not running on cluster, skipping rest of this test.")
+            return
+
+        actor_node_ips = [node_ips[actor_nodes[nid]] for nid in actor_nodes]
+
+        # Initialize data frames on remote nodes
+        # This way we can control which partition is on which node
+        def create_remote_df(arr):
+            return ray.put(pd.DataFrame(arr))
+
+        partitions = np.split(self.x, len(part_nodes))
+        node_dfs = ray.get([
+            ray.remote(create_remote_df).options(resources={
+                f"node:{nip}": 0.1
+            }).remote(partitions[pid])
+            for pid, nip in enumerate(actor_node_ips)
+        ])
+        node_ip_dfs = [(ray.put(f"node{part_nodes[pid]}"), node_df)
+                       for pid, node_df in enumerate(node_dfs)]
+
+        # Create modin dataframe from distributed partitions
+        from modin.distributed.dataframe.pandas import from_partitions
+        modin_df = from_partitions(node_ip_dfs, axis=0)
+
+        # Create ray actors
+        actors = [
+            RayXGBoostActor.options(resources={
+                f"node:{nip}": 0.1
+            }).remote(rank=rank, num_actors=len(actor_nodes))
+            for rank, nip in enumerate(actor_node_ips)
+        ]
+
+        # Calculate shards
+        actor_to_parts = Modin.get_actor_shards(modin_df, actors)
+
+        for actor_rank, part_ids in expected_actor_parts.items():
+            for i, part_id in enumerate(part_ids):
+                assigned_df = ray.get(actor_to_parts[actor_rank][i])
+                part_df = pd.DataFrame(partitions[part_id])
+
+                self.assertTrue(
+                    assigned_df.equals(part_df),
+                    msg=f"Assignment failed: Actor rank {actor_rank}, "
+                    f"partition {i} is not partition with ID {part_id}.")
+
     def testAssignEvenTrivial(self):
         """Assign actors to co-located partitions, trivial case.
 
         In this test case, partitions are already evenly distributed across
         nodes.
         """
-        part_nodes = [
-            "node0",
-            "node0",
-            "node1",
-            "node1",
-            "node2",
-            "node2",
-            "node3",
-            "node3",
-        ]
-        actors_to_node = {
-            0: "node0",
-            1: "node1",
-            2: "node2",
-            3: "node3",
-        }
+        part_nodes = [0, 0, 1, 1, 2, 2, 3, 3]
+        actor_nodes = [0, 1, 2, 3]
+
         expected_actor_parts = {
             0: [0, 1],
             1: [2, 3],
             2: [4, 5],
             3: [6, 7],
         }
-        self._testAssignPartitions(part_nodes, actors_to_node,
+        self._testAssignPartitions(part_nodes, actor_nodes,
                                    expected_actor_parts)
+        self._testModinAssignment(part_nodes, actor_nodes,
+                                  expected_actor_parts)
 
     def testAssignEvenRedistributeOne(self):
         """Assign actors to co-located partitions, non-trivial case.
@@ -91,88 +135,56 @@ class ModinDataSourceTest(unittest.TestCase):
         with co-located partitions. The other one has to fetch data from
         node1.
         """
-        part_nodes = [
-            "node0",
-            "node0",
-            "node0",
-            "node1",
-            "node1",
-            "node1",
-            "node2",
-            "node2",
-        ]
-        actors_to_node = {
-            0: "node0",
-            1: "node0",
-            2: "node1",
-            3: "node2",
-        }
+        part_nodes = [0, 0, 0, 1, 1, 1, 2, 2]
+        actor_nodes = [0, 0, 1, 2]
+
         expected_actor_parts = {
             0: [0, 2],
             1: [1, 5],
             2: [3, 4],
             3: [6, 7],
         }
-        self._testAssignPartitions(part_nodes, actors_to_node,
+        self._testAssignPartitions(part_nodes, actor_nodes,
                                    expected_actor_parts)
+        self._testModinAssignment(part_nodes, actor_nodes,
+                                  expected_actor_parts)
 
     def testAssignEvenRedistributeMost(self):
         """Assign actors to co-located partitions, redistribute case.
 
         In this test case, partitions are all on one node.
         """
-        part_nodes = [
-            "node0",
-            "node0",
-            "node0",
-            "node0",
-            "node0",
-            "node0",
-            "node0",
-            "node0",
-        ]
-        actors_to_node = {
-            0: "node0",
-            1: "node1",
-            2: "node2",
-            3: "node3",
-        }
+        part_nodes = [0, 0, 0, 0, 0, 0, 0, 0]
+        actor_nodes = [0, 1, 2, 3]
+
         expected_actor_parts = {
             0: [0, 1],
             1: [2, 5],
             2: [3, 6],
             3: [4, 7],
         }
-        self._testAssignPartitions(part_nodes, actors_to_node,
+        self._testAssignPartitions(part_nodes, actor_nodes,
                                    expected_actor_parts)
+        self._testModinAssignment(part_nodes, actor_nodes,
+                                  expected_actor_parts)
 
     def testAssignUnevenTrivial(self):
         """Assign actors to co-located partitions, trivial uneven case.
 
         In this test case, not all actors get the same amount of partitions.
         """
-        part_nodes = [
-            "node0",
-            "node0",
-            "node0",
-            "node1",
-            "node1",
-            "node2",
-            "node2",
-            "node2",
-        ]
-        actors_to_node = {
-            0: "node0",
-            1: "node1",
-            2: "node2",
-        }
+        part_nodes = [0, 0, 0, 1, 1, 2, 2, 2]
+        actor_nodes = [0, 1, 2]
+
         expected_actor_parts = {
             0: [0, 1, 2],
             1: [3, 4],
             2: [5, 6, 7],
         }
-        self._testAssignPartitions(part_nodes, actors_to_node,
+        self._testAssignPartitions(part_nodes, actor_nodes,
                                    expected_actor_parts)
+        self._testModinAssignment(part_nodes, actor_nodes,
+                                  expected_actor_parts)
 
     def testAssignUnevenRedistribute(self):
         """Assign actors to co-located partitions, redistribute uneven case.
@@ -180,28 +192,18 @@ class ModinDataSourceTest(unittest.TestCase):
         In this test case, not all actors get the same amount of partitions.
         Some actors have to fetch partitions from other nodes
         """
-        part_nodes = [
-            "node0",
-            "node0",
-            "node1",
-            "node1",
-            "node1",
-            "node1",
-            "node2",
-            "node4",
-        ]
-        actors_to_node = {
-            0: "node0",
-            1: "node1",
-            2: "node2",
-        }
+        part_nodes = [0, 0, 1, 1, 1, 1, 2, 4]
+        actor_nodes = [0, 1, 2]
+
         expected_actor_parts = {
             0: [0, 1, 5],
             1: [2, 3, 4],
             2: [6, 7],
         }
-        self._testAssignPartitions(part_nodes, actors_to_node,
+        self._testAssignPartitions(part_nodes, actor_nodes,
                                    expected_actor_parts)
+        self._testModinAssignment(part_nodes, actor_nodes,
+                                  expected_actor_parts)
 
 
 if __name__ == "__main__":
