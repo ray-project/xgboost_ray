c02299dedf563d7c4639a1effe9e4f1701eb42b1
Kai Fricke <kai@anyscale.com>
2021-05-20 19:53:10 +0100

    Bump to v0.1.1 for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="xgboost_ray",
     packages=find_packages(where=".", include="xgboost_ray*"),
-    version="0.1.0",
+    version="0.1.1",
     author="Ray Team",
     description="A Ray backend for distributed XGBoost",
     long_description="A distributed backend for XGBoost built on top of "
