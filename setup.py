92f7dec12ca3584fb7f81dba5b93a2ecbd25a76d
Antoni Baum <antoni.baum@protonmail.com>
2022-02-16 19:19:03 +0100

    Bump to 0.1.8 for future development (#191)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="xgboost_ray",
     packages=find_packages(where=".", include="xgboost_ray*"),
-    version="0.1.7",
+    version="0.1.8",
     author="Ray Team",
     description="A Ray backend for distributed XGBoost",
     long_description="A distributed backend for XGBoost built on top of "
