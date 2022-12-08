fa9b08cd2c06055f267e634ac9b76288416d63e6
Antoni Baum <antoni.baum@protonmail.com>
2022-12-08 22:49:22 +0000

    Lint
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- xgboost_ray/tune.py
+++ xgboost_ray/tune.py
@@ -122,7 +122,8 @@ def _get_tune_resources(num_actors: int, cpus_per_actor: int,
         bundles = [head_bundle] + child_bundles
         placement_options = placement_options or {}
         placement_options.setdefault("strategy", "PACK")
-        # Special case, same as in ray.air.ScalingConfig.as_placement_group_factory
+        # Special case, same as in
+        # ray.air.ScalingConfig.as_placement_group_factory
         # TODO remove after Ray 2.3 is out
         if placement_options.get("_max_cpu_fraction_per_node", None) is None:
             placement_options.pop("_max_cpu_fraction_per_node", None)
