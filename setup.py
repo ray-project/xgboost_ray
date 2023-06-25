8daed68de24f04a27d8c8b42b0da35fd958dd5d7
Antoni Baum <antoni.baum@protonmail.com>
2023-06-25 04:37:26 -0700

    Bump version to 0.1.17 (#282)
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="xgboost_ray",
     packages=find_packages(where=".", include="xgboost_ray*"),
-    version="0.1.16",
+    version="0.1.17",
     author="Ray Team",
     description="A Ray backend for distributed XGBoost",
     license="Apache 2.0",
