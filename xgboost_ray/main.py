7f4ad7d45e19e8eb37dedcefa76b48c7a5fcd93a
Justin Yu <justinvyu@anyscale.com>
2023-12-28 09:52:32 -0800

    remove ipdb typo
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- xgboost_ray/main.py
+++ xgboost_ray/main.py
@@ -1489,9 +1489,6 @@ def train(
             "order to use xgboost_ray with Tune."
         )
 
-    import ipdb
-
-    ipdb.set_trace()
     if added_tune_callback or get_current_placement_group():
         # Don't autodetect resources when used with Tune.
         cpus_per_actor = ray_params.cpus_per_actor
